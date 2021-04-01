ee12d0e96255c7dad97066387a33089f98fb5dcf
Oliver Ilmjärv <oilmjarv@erply.com>
2021-04-01 16:56:09 +0300

    add new stricture for general api request

---
+++ src/components/requests/requestLists/main.ts
@@ -0,0 +1,13 @@
+export interface RequestJsonI {
+	baseUrl?: string;
+	request: string;
+	path: string;
+	method: 'POST' | 'GET' | 'PUT' | 'PATCH' | 'DELETE';
+	description: string;
+	fields: {
+		name: string;
+		type: 'select' | 'string' | 'number';
+		options?: string[];
+		helper?: string;
+	}[];
+}
